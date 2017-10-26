15dd6a8c3481d00a3646558a2cd9abafc0324d75
k23Burke <k23burke@gmail.com>
2017-10-26 15:36:07 -0700

    build for import into projects

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
+
+gulp.task('default', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './src/index.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(gulp.dest('./build/'));
+});
