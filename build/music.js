56a9d4c18935c189345daa7e84459768664ffa50
k23Burke <k23burke@gmail.com>
2017-12-01 11:42:40 -0500

    order music recordings by position

--- build/music.js
+++ build/music.js
@@ -54,6 +54,7 @@ inherit(MusicRecording, Manifestation);
 propArray(MusicRecording, new MusicGroup(), 'byArtist');
 propValue(MusicRecording, String(), 'isrcCode');
 propValue(MusicRecording, new MusicComposition(), 'recordingOf');
+propValue(MusicRecording, Number(), 'position');
 
 // MusicAlbum
 
